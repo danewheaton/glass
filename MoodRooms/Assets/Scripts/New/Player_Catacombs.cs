83a77a2321bb451a657c1ce81c3f3b0c3c1deb9a
IAM\james.robertson <wheaton.dane@gmail.com>
2017-04-26 16:55:49 -0500

    catacombs

--- MoodRooms/Assets/Scripts/New/Player_Catacombs.cs
+++ MoodRooms/Assets/Scripts/New/Player_Catacombs.cs
@@ -2,18 +2,83 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
+public enum WhichMirror { FIRST, SECOND, THIRD, FOURTH, FIFTH }
+
 public class Player_Catacombs : MonoBehaviour
 {
     [SerializeField]
-    GameObject gabe, gabeTrigger, disappearingWall, end;
+    GameObject gabe, gabeTrigger, disappearingWall, end, mirror1, mirror2, mirror3, mirror4, mirror5, doorblocker;
 
     [SerializeField]
-    AudioSource jumpscare, music;
+    AudioSource jumpscare, music, doorCreak;
 
-    bool facingGabe, inTrigger, hasPlayedJumpscare, highEnough;
+    GameObject mirror;
+
+    WhichMirror whichMirror = WhichMirror.FIRST;
+
+    bool facingGabe, inTrigger, hasPlayedJumpscare, highEnough, facingMirror;
+
+    void Start()
+    {
+        Camera.main.cullingMask = ((1 << LayerMask.NameToLayer("Default")) | ((1 << LayerMask.NameToLayer("TransparentFX")) |
+                    (1 << LayerMask.NameToLayer("Ignore Raycast")) | (1 << LayerMask.NameToLayer("Water")) | (1 << LayerMask.NameToLayer("UI"))));
+
+        mirror = mirror1;
+    }
 
     private void Update()
     {
+        //print(Vector3.Distance(transform.position, mirror.transform.position));
+        //print(Vector3.Angle(mirror.transform.position, -transform.forward));
+        
+        facingMirror = Vector3.Angle(mirror.transform.position, -transform.forward) <= 90 && Vector3.Distance(transform.position, mirror.transform.position) < 3;
+
+        if (facingMirror)
+        {
+            switch (whichMirror)
+            {
+                case WhichMirror.FIRST:
+                    Camera.main.cullingMask = ((1 << LayerMask.NameToLayer("Default")) | ((1 << LayerMask.NameToLayer("TransparentFX")) |
+                    (1 << LayerMask.NameToLayer("Ignore Raycast")) | (1 << LayerMask.NameToLayer("Water")) | (1 << LayerMask.NameToLayer("UI")) |
+                    (1 << LayerMask.NameToLayer("Glass1"))));
+                    mirror = mirror2;
+                    whichMirror = WhichMirror.SECOND;
+                    break;
+                case WhichMirror.SECOND:
+                    Camera.main.cullingMask = ((1 << LayerMask.NameToLayer("Default")) | ((1 << LayerMask.NameToLayer("TransparentFX")) |
+                    (1 << LayerMask.NameToLayer("Ignore Raycast")) | (1 << LayerMask.NameToLayer("Water")) | (1 << LayerMask.NameToLayer("UI")) |
+                    (1 << LayerMask.NameToLayer("Glass1")) | (1 << LayerMask.NameToLayer("Door01"))));
+                    mirror = mirror3;
+                    whichMirror = WhichMirror.THIRD;
+                    break;
+                case WhichMirror.THIRD:
+                    Camera.main.cullingMask = ((1 << LayerMask.NameToLayer("Default")) | ((1 << LayerMask.NameToLayer("TransparentFX")) |
+                    (1 << LayerMask.NameToLayer("Ignore Raycast")) | (1 << LayerMask.NameToLayer("Water")) | (1 << LayerMask.NameToLayer("UI")) |
+                    (1 << LayerMask.NameToLayer("Glass1")) | (1 << LayerMask.NameToLayer("Door01")) | (1 << LayerMask.NameToLayer("Door02"))));
+                    mirror = mirror4;
+                    whichMirror = WhichMirror.FOURTH;
+                    break;
+                case WhichMirror.FOURTH:
+                    Camera.main.cullingMask = ((1 << LayerMask.NameToLayer("Default")) | ((1 << LayerMask.NameToLayer("TransparentFX")) |
+                    (1 << LayerMask.NameToLayer("Ignore Raycast")) | (1 << LayerMask.NameToLayer("Water")) | (1 << LayerMask.NameToLayer("UI")) |
+                    (1 << LayerMask.NameToLayer("Glass1")) | (1 << LayerMask.NameToLayer("Door01")) | (1 << LayerMask.NameToLayer("Door02")) |
+                    (1 << LayerMask.NameToLayer("Portal01"))));
+                    mirror = mirror5;
+                    whichMirror = WhichMirror.FIFTH;
+                    break;
+                case WhichMirror.FIFTH:
+                    Camera.main.cullingMask = ((1 << LayerMask.NameToLayer("Default")) | ((1 << LayerMask.NameToLayer("TransparentFX")) |
+                    (1 << LayerMask.NameToLayer("Ignore Raycast")) | (1 << LayerMask.NameToLayer("Water")) | (1 << LayerMask.NameToLayer("UI")) |
+                    (1 << LayerMask.NameToLayer("Glass1")) | (1 << LayerMask.NameToLayer("Door01")) | (1 << LayerMask.NameToLayer("Door02")) |
+                    (1 << LayerMask.NameToLayer("Portal01")) | (1 << LayerMask.NameToLayer("Glass2"))));
+                    doorblocker.SetActive(false);
+                    doorCreak.Play();
+                    break;
+                default:
+                    break;
+            }
+        }
+
         facingGabe = Vector3.Angle(gabe.transform.position, transform.forward) >= 60;
 
         if (facingGabe && inTrigger)
