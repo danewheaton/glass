e4580114a9892c37b0a321f62d2735c9188a90ab
Dane Wheaton <wheaton.dane@gmail.com>
2017-04-26 22:20:34 -0500

    no message

--- MoodRooms/Assets/Scripts/New/PlayerTeleportation.cs
+++ MoodRooms/Assets/Scripts/New/PlayerTeleportation.cs
@@ -30,7 +30,7 @@ public class PlayerTeleportation : MonoBehaviour
         doorToCatacombs, doorToCatacombsTrigger, doorAtBottomOfStairwell, doorAtBottomOfStairwellTrigger,
         doorAtBottomOfStairwellBlocker, catacombsUnlit, catacombsLit, endTrigger, staticAssets, dynamicAssets, observatory1, observatory2;
     public GameObject[] upperHallway, glassPortals, scrawlings, disappearingPassage, reappearingNook, observatoryMirrors;
-    public Transform refectoryWeenie, startingDoorTransform, teleporter02Transform, glass1Transform, portal01Transform, mirror01Transform, glassShardTransform, fallingPortalCamTransform;
+    public Transform refectoryWeenie, middle, startingDoorTransform, teleporter02Transform, glass1Transform, portal01Transform, mirror01Transform, glassShardTransform, fallingPortalCamTransform;
     public Transform[] playerStarts;
     public Rigidbody[] glassRigidBodies, moreGlassRbs;
     public Collider[] stairColliders;
@@ -207,6 +207,7 @@ public class PlayerTeleportation : MonoBehaviour
                 originalChime = creditsPanel.soundFeed.warpSound;
                 creditsPanel.soundFeed.warpSound = victory;
                 StartCoroutine(creditsPanel.FlashWhite());
+                transform.position = middle.position;
                 GameObject[] remainingShards = GameObject.FindGameObjectsWithTag("Oculus");
                 foreach (GameObject g in remainingShards) g.SetActive(false);
             }
